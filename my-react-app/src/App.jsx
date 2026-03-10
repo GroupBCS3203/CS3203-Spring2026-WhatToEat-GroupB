f6442b3df26054abbc7b5d7cc5699308ac0e3410
Hicksn <noahfhicks@gmail.com>
2026-03-10 10:41:11 -0500

    React frontend to be deployed.

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,81 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { useRef } from "react";
+
+
+function Button({ onClick, children }) {
+  return (
+    <button className='button' onClick={onClick}>
+      {children}
+    </button>
+  );
+}
+
+
+function Tabbutton({ feature }) {
+
+  function handlePlayClick() {
+    var i, tabcontent;
+
+      // Get all elements with class="tabcontent" and hide them
+      tabcontent = document.getElementsByClassName("tabcontent");
+      for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+      }
+       document.getElementById(feature).style.display = "block";
+  }
+
+  return (
+    <Button onClick={handlePlayClick}>
+     {feature}
+    </Button>
+  );
+}
+
+
+function App() {
+
+  function openFeature(feature) {
+      
+       
+    }
+    
+
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+
+
+      <div className="tab">
+          <Tabbutton feature = "recipes" />
+          <Tabbutton feature = "login" />
+          <Tabbutton feature = "planner" />
+        </div>
+
+        <div id="recipes" className="tabcontent" style={{display: "block"}}>
+          <h3>Recipe Browser</h3>
+          <p>placeholder.</p>
+        </div>
+
+        <div id="login" className="tabcontent" style={{display: "none"}}>
+          <h3>Login</h3>
+          <p>placeholder.</p>
+        </div>
+
+        <div id="planner" className="tabcontent" style={{display: "none"}}>
+          <h3>Meal Planner</h3>
+          <p>placeholder.</p>
+        </div>
+    </>
+  )
+
+
+  
+
+}
+
+export default App
