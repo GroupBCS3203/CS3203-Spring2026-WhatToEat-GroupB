e25d305ae502d8f444c31be0e36380fd88a61d33
Matbenhou <mbhouston123@icloud.com>
2026-05-02 14:18:44 -0500

    rebased the dev branch to use the latest version of main

--- my-react-app/src/recipeFinder.jsx
+++ my-react-app/src/recipeFinder.jsx
@@ -1,10 +1,12 @@
 import {useEffect, useState} from "react";
 import {Button} from "./App.jsx";
+import { createPortal } from "react-dom";
 
 export function RecipeFinder()
 {
     const [recipes, setRecipes] = useState([]);
     const [searchTerm, setSearchTerm] = useState('');
+    const [showPopup, setShowPopup] = useState(false);
 
     useEffect(() => {
         fetch(`${import.meta.env.VITE_API_URL}/api/recipes/top`)
@@ -37,9 +39,25 @@ export function RecipeFinder()
         }
     }
 
+    let Popup =
+        (
+            <div style={styles.popup}>
+                <h3 style={{ color:'#ffffff' }}>
+                    Recipe Browser
+                </h3>
+                <input
+                    type="text"
+                    placeholder="Search here..."
+                    onChange={handleInputChange} // Attach the onChange event handler
+                    value={searchTerm} // Control the input value with state
+                />
+            </div>
 
-    return(<div>
-        <h3 style={{ color:'#ffffff' }}>
+        );
+
+
+return(<div>
+        <h3 style={{ cursor: "pointer" , color:'#ffffff',  textDecoration: 'underline' }} onClick={() => setShowPopup(true)}>
             Recipe Browser
         </h3>
         <input
@@ -52,6 +70,19 @@ export function RecipeFinder()
             Get 10 Recipes
         </Button>
 
+        {showPopup &&
+        createPortal(
+            Popup,
+            document.body
+        )}
+
+        <button onClick={() => setShowPopup(true)}>Open Popup</button>
+        <button onClick={() => setShowPopup(false)}> Press me to print a message! </button>
+
+        <Button onClick={() => Popup()}>
+            Get 20 Recipes
+        </Button>
+
         {recipes.length === 0 ? (
             <p>Loading...</p>
         ) : (
@@ -61,3 +92,15 @@ export function RecipeFinder()
         )}
     </div>)
 }
+
+const styles = {
+    popup: {
+        position: "fixed",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            background: "white",
+            padding: "20px",
+            zIndex: 9999,
+    },
+};
